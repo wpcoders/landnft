a43433cb8f97982cbf502d7e898ef24eb0aa1979
wpcoders <gauravisonline@gmail.com>
2022-12-21 17:15:01 +0530

    added test command under npm.

--- test/LandSale.js
+++ test/LandSale.js
@@ -93,10 +93,9 @@ describe("CraftdefiSLands", async function () {
     const { landSaleToken, CraftdefiSLandsToken } = await loadFixture(
       deployTokenFixture
     );
-      console.log("landSaleToken.address", landSaleToken.address);
+
     const nGrantRoleTx = await CraftdefiSLandsToken.grantRole("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", landSaleToken.address);
     await nGrantRoleTx.wait();
-
   });
 
   // it("should only enabled zone can be minted", async function () {
